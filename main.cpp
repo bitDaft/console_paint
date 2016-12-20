100b3f887d1778c7806041d7bc3d1c81a89fc6d3
meow-man <anodecode@gmail.com>
2016-12-20 19:39:11 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/** an improved paint application with user friendly interface and functionality **/
+
+/** features sketch
+
+new save delete open edit-implicitly impiled
+
+
+
+**/
+
+#include <windows.h>
+#include <iostream>
+#include <conio.h>
+#include "consoleMenu.h"
+
+using namespace std;
+
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
